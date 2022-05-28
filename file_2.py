19cbf47c9542af9aa368dc38db53377faaaa35e5
liricooli <liricooli@gmail.com>
2022-05-28 17:42:05 +0300

    changed file_2

--- file_2.py
+++ file_2.py
@@ -1,12 +1,10 @@
 print("The greatest glory in living lies not in never falling, but in rising every time")
 
-def def_in_function_diff_base(a, b):
-    a + 1
-    a + 1
-    return b
-
-
 def def2_in_function_diff_base(c, d):
-    c + 1
     d + 1
     return d
+
+
+def new_def_in_function_diff_base(e, f):
+    e + 1
+    return f
