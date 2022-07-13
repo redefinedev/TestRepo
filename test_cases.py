e3e76f1416643cd43a4069e419eff4c8e69a6ec4
Ayal Regev <ayal@redefine.dev>
2022-07-13 09:47:02 +0300

    added duration for tests

--- test_cases.py
+++ test_cases.py
@@ -1,11 +1,12 @@
+from time import sleep
+
 import pytest
 
-def test_passes():
-  pass
 
-def test_fails():
-  assert 1 == 0
+@pytest.fixture()
+def fixture_sleep():
+    sleep(1)
 
-@pytest.mark.skip
-def test_skipped():
-  assert 1 == 2
+
+def test_duration():
+    assert 0 == 0
