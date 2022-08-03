995ba0df97c30d2f5946dfe497b5edb47920b5c3
Nitay <nitay@redefine.dev>
2022-08-03 17:31:37 +0300

    aaaaaa

--- calculator/run.sh
+++ calculator/run.sh
@@ -3,5 +3,5 @@ export REDEFINE_GO_PATH=/usr/local/go/bin/go
 export REDEFINE_DEBUG_MODE=true
 export REDEFINE_LOG_PATH=./Logs/
 
-cp ../../../../agents/road_runner/go_test/go ./calculator
+cp ../../../../agents/road_runner/go_test/go ./
 ./go test -json -v
