e122ef63aee021d1a7d5ec2cb965963e62d61cd5
Nitay <nitay@redefine.dev>
2022-08-08 11:19:42 +0300

    no logs

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 export REDEFINE_GO_PATH=/usr/local/go/bin/go
-export REDEFINE_DEBUG_MODE=true
-export REDEFINE_LOG_PATH=./Logs/
+export REDEFINE_DEBUG_MODE=false
 export REDEFINE_ANONYMIZATION=false
 
-rm ./Logs/*
 cp $1 ./
 ./go test -json -v ./...
