e44ff4f7d1be954da457090c9d85d0c2080f16b3
nitayH <nitay@redefine.dev>
2023-01-08 12:34:44 +0200

    docker test

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.7.10 as test-repo
+
+ARG POETRY_DYNAMIC_VERSIONING_BYPASS="0.0.0"
+# This is needed so the var is accessible by the entrypoint
+ENV POETRY_DYNAMIC_VERSIONING_BYPASS=$POETRY_DYNAMIC_VERSIONING_BYPASS
+ENV REDEFINE_ENVIRONMENT="dev"
+ENV REDEFINE_AUTH="6a71bb1b-fdd6-4f1b-94e9-08e64a3ee537::0e6e3a6e-1071-4a95-9467-8c5f3f124606"
+
+RUN mkdir /redefine_artifacts
+RUN mkdir /entrypoint_script_folder
+
+RUN mkdir /project
+WORKDIR /project
+
+COPY installer_docker_entrypoint.sh /entrypoint_script_folder/installer_docker_entrypoint.sh
+RUN chmod +x /entrypoint_script_folder/installer_docker_entrypoint.sh
+
+
+# run installer, need configuration via env or the code itself with config
+ENTRYPOINT [ "/entrypoint_script_folder/installer_docker_entrypoint.sh" ]
+# set the default source branch argument for the script
+CMD ["-s", "feature/pytests_for_teamcity"]
---
+++ installer_docker_entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+while getopts "s:g:" flag
+do
+    case "${flag}" in
+        s) source_branch=${OPTARG};;
+        g) git_args=${OPTARG};;
+    esac
+done
+
+echo "source branch: ${source_branch}"
+echo "git_args: ${git_args}"
+echo "artifacts version used:"
+
+git clone -q https://github.com/redefinedev/TestRepo -b ${source_branch} ${git_args}
+(cd TestRepo && git remote set-branches origin ${source_branch})
+pip install -q pytest
+
+# installing and running redefine 
+export REDEFINE_AUTH="6a71bb1b-fdd6-4f1b-94e9-08e64a3ee537::0e6e3a6e-1071-4a95-9467-8c5f3f124606"
+pip install redefine-cli --extra-index-url=https://redefine.dev/pip/
+
+redefine config set cert_path="" 
+redefine start --verbose --collect-only
+
+# for logger use for python components tests
+touch /project/TestRepo/.env.development
+echo 'REDEFINE_ENVIRONMENT="dev"' >> /project/TestRepo/.env.development
+echo 'REDEFINE_AUTH="6a71bb1b-fdd6-4f1b-94e9-08e64a3ee537::0e6e3a6e-1071-4a95-9467-8c5f3f124606"' >> /project/TestRepo/.env.development
+
+# used as a marker to indicate that the container is ready
+echo "container ready"
+
+pytest /project/TestRepo
+
+# a docker container exits after the entrypoint script finishes
+# this makes sure it stays up and running so we can query it from the tests
+while true; do sleep 1000; done
