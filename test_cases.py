336dec3e277a9afb416962ff8bc1204fd82692ac
nitayH <nitay@redefine.dev>
2023-03-28 13:11:43 +0300

    added parametrize test

--- test_cases.py
+++ test_cases.py
@@ -1,11 +1,5 @@
 import pytest
 
-def test_passes():
-  pass
-
-def test_fails():
-  assert 1 == 0
-
-@pytest.mark.skip
-def test_skipped():
-  assert 1 == 2
+@pytest.mark.parametrize("i", [i for i in range(30000)])
+def test_parameter(i:int):
+  assert(i % 2 == 0)
