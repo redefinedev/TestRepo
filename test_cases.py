f17df5b21f9e7f3a00aed7243b446c0182fdbd7a
nitayH <nitay@redefine.dev>
2023-04-02 11:30:28 +0300

    ahhh

--- test_cases.py
+++ test_cases.py
@@ -1,39 +1,25 @@
-import time
 import pytest
 
-@pytest.fixture(scope="function")
-def sleep_fixture():
-  print("in fixture")
-  time.sleep(3)
-
-
-def test_1_sec_pass(sleep_fixture):
-  time.sleep(1)
+def pass_0():
   assert True
 
-def test_1_sec_fail():
-  time.sleep(1)
+def fail_0():
   assert 1==0
 
-def test_2_sec_pass():
-  time.sleep(2)
+def pass_1():
   assert True
 
-def test_2_sec_fail():
-  time.sleep(2)
+def fail_1():
   assert 1==0
 
-def test_10_seconds():
-  time.sleep(10)
+def pass_2():
   assert True
 
-def test_another_1_sec():
-  time.sleep(1)
+def pass_3():
   assert True
 
-def test_skip():
-  pytest.skip()
-
-@pytest.mark.parametrize("number", [i for i in range(30,000)])
-def a_lot_of_tests(number):
+def pass_4():
   assert True
+
+def fail_2():
+  assert 1==0
