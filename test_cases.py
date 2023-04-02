3fd245fdb5e2d4dfe6d272efc21973af4998048f
nitayH <nitay@redefine.dev>
2023-04-02 15:01:32 +0300

    added skip

--- test_cases.py
+++ test_cases.py
@@ -3,23 +3,28 @@ import pytest
 def test_pass_0():
   pass
 
-def test_fail_0():
-  assert 1==0
-
-def test_pass_1():
-  pass
-
 def test_fail_1():
   assert 1==0
+
+@pytest.mark.skip()
+def test_skip():
+  pass
 
 def test_pass_2():
   pass
 
-def test_pass_3():
-  pass
+def test_fail_3():
+  assert 1==0
 
 def test_pass_4():
   pass
 
-def test_fail_2():
+def test_pass_5():
+  pass
+
+def test_pass_6():
+  pass
+
+def test_fail_7():
   assert 1==0
+
