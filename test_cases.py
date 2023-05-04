851d8d46d13dc58dcf99e6e0cf406021e45dfd0f
nitayH <nitay@redefine.dev>
2023-05-04 16:04:47 +0300

    .

--- test_cases.py
+++ test_cases.py
@@ -1,12 +1,12 @@
 import pytest
 
-@pytest.mark.skip(reason="")
-def test_skip_1():
-  pass
-
 @pytest.mark.parametrize("i", [i for i in range(9000)])
 def test_parameter(i:int):
   assert(i % 2 == 0)
+
+@pytest.mark.skip(reason="")
+def test_skip_1():
+  pass
 
 @pytest.mark.skip(reason="")
 def test_skip():
