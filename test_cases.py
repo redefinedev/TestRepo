b2b84115cea69cb5d01b2ae42910015af90118de
liricooli <liricooli@gmail.com>
2023-05-08 17:13:57 +0300

    WIP

--- test_cases.py
+++ test_cases.py
@@ -4,9 +4,10 @@ import time
 
 @pytest.fixture()
 async def dune_args():
+    print("fixture")
     raise RuntimeError("This is an error")
 
-def test_pass_0():
+def test_pass_0(dune_args):
   time.sleep(2)
   pass
 
