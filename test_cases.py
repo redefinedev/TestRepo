59167a8ad84560a5268bae65243965cc7dce1879
nitayH <nitay@redefine.dev>
2023-05-08 19:21:57 +0300

    .

--- test_cases.py
+++ test_cases.py
@@ -1,13 +1,5 @@
 import pytest
 
-@pytest.fixture(scope="function")
-def fail_fixture():
-  raise Exception("oh no some error happened")
-
-
-def test_set_fail(fail_fixture):
-  pass
-
 @pytest.mark.parametrize("i", [i for i in range(9000)])
 def test_parameter(i:int):
   assert(i % 2 == 0)
