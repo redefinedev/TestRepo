2ede1be6312d41767571d9fa4d5ccee44d80c5a5
nitayH <nitay@redefine.dev>
2023-05-09 10:12:56 +0300

    .

--- test_cases.py
+++ test_cases.py
@@ -1,16 +1,20 @@
 import pytest
 
 @pytest.fixture(scope="session")
-def fail_fixture():
+def outer_fail_fixture():
+  raise Exception("oh no some error happened")
+
+@pytest.fixture(scope="function")
+def inner_fail_fixture():
   raise Exception("oh no some error happened")
 
 
-def test_set_fail(fail_fixture):
+def test_set_fail(inner_fail_fixture):
   pass
 
-def test_set_fail_1(fail_fixture):
+def test_set_fail_1(inner_fail_fixture):
   pass
 
-def test_set_fail_2(fail_fixture):
+def test_set_fail_2(inner_fail_fixture):
   pass
 
