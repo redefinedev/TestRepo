c5980011ef3f40621476efe3bd79d2650994de88
Ayal Regev <ayal@redefine.dev>
2023-05-15 20:08:33 +0300

    elapsed time tests

--- test_cases.py
+++ test_cases.py
@@ -1,11 +1,17 @@
+from time import sleep
+
 import pytest
 
-def test_passes():
-  pass
 
-def test_fails():
-  assert 1 == 0
+def test_fail_1():
+    sleep(1)
+    assert False
+
+
+def test_fail_2():
+    assert False
+
 
 @pytest.mark.skip
 def test_skipped():
-  assert 1 == 2
+    assert 1 == 2
