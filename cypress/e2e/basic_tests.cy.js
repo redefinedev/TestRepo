27e49db3dd0cd47b6a03443e3d6898286d9867b6
britt frenkel <britt@redefine.dev>
2023-05-17 10:59:08 +0300

    initial cypress prep

---
+++ cypress/e2e/basic_tests.cy.js
@@ -0,0 +1,7 @@
+/* add tests before and after and check the time to first failure parameter */
+describe("suite with failing test", () => {
+    it("failing test", () => {
+        expect(true).to.equal(false);
+    });
+    }
+);
