b006d731ca25178361a041dc01e457f034f199a2
britt frenkel <britt@redefine.dev>
2023-05-20 14:28:05 +0300

    added basic test

--- cypress/e2e/basic_tests.cy.js
+++ cypress/e2e/basic_tests.cy.js
@@ -1,5 +1,16 @@
+
 /* add tests before and after and check the time to first failure parameter */
 describe("suite with failing test", () => {
+    it("first test not failing", () => {
+        expect(true).to.equal(true);
+        cy.wait(1000);
+    });
+
+    it("second test not failing", () => {
+        expect(true).to.equal(true);
+        cy.wait(1000);
+    });
+
     it("failing test", () => {
         expect(true).to.equal(false);
     });
