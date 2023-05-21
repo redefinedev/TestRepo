b6a93cfb3fbd7b14251de7bf280c41d92947f534
britt frenkel <britt@redefine.dev>
2023-05-21 12:43:54 +0300

    added log to config

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       config = require("./Redefine.js").redefinePlugin(on, config);
+      console.log("received spec files to run - ",  config?.specPattern)
       return config;
     },
   },
