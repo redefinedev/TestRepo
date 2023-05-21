459e4ba3a5bb892c397aaf09ebfd1b332c06fe37
Ayal Regev <ayal@redefine.dev>
2023-05-21 16:52:23 +0300

    xdist_scope_tests

--- test_cases.py
+++ test_cases.py
@@ -1,3 +1,4 @@
+""" Runs with 2 nodes. Without --dist=loadscope expects to be mixed(both fixture prints), but with --dist=loadscope it is not mixed. """
 import pytest
 
 
@@ -6,42 +7,21 @@ def write(text):
         f.write(text + "\n")
 
 
-@pytest.fixture(scope="session")
-def fix_s1():
-    write("fix_s1")
-
-
-@pytest.fixture(scope="module")
-def fix_m1():
-    write("fix_m1")
-
-
-def test_ayal():
-    write("test_ayal")
-
-
 class TestClass1:
-    @pytest.fixture
+    @pytest.fixture(scope="class")
     def fix_c1(self):
         write("fix_c1")
 
-    def test_c11(self, fix_c1, fix_s1, fix_m1):
-        write("c11")
-
-    def test_c12(self, fix_c1, fix_s1, fix_m1):
-        write("c12")
-        assert False
+    @pytest.mark.parametrize("i", range(100))
+    def test_c11(self, fix_c1, i):
+        assert True
 
 
 class TestClass2:
-    @pytest.fixture(scope="session")
+    @pytest.fixture(scope="class")
     def fix_c2(self):
         write("fix_c2")
 
-    def test_c21(self, fix_c2, fix_s1, fix_m1):
-        write("c21")
-
-    def test_c22(self, fix_c2, fix_s1, fix_m1):
-        write("c22")
-        assert False
-
+    @pytest.mark.parametrize("i", range(100))
+    def test_c2(self, fix_c2, i):
+        assert True
