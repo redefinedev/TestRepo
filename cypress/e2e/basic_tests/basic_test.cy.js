5668801ce4c5a2a440ffe6be59132b9fa2eafff1
britt frenkel <britt@redefine.dev>
2023-05-21 11:17:15 +0300

    created testing dir for basic tests

---
+++ cypress/e2e/basic_tests/basic_test.cy.js
@@ -0,0 +1,9 @@
+
+describe("basic suite test", () => {
+    it("first test not failing", () => {
+        expect(true).to.equal(true);
+        cy.wait(1000);
+    });
+
+    }
+);
---
+++ cypress/e2e/basic_tests/failing_test.cy.js
@@ -0,0 +1,9 @@
+
+describe("suite with failing test", () => {
+
+    it("failing test", () => {
+	    // this test should fail
+        expect(true).to.equal(false);
+    });
+    }
+);
