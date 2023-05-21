68950c097430f69e6b9c95f92589e5a8240adcf6
britt frenkel <britt@redefine.dev>
2023-05-21 13:57:02 +0300

    trying to run async funcs

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
       // implement node event listeners here
-      config = require("./Redefine.js").redefinePlugin(on, config);
-      console.log("received spec files to run - ",  config?.specPattern)
+      config = await require("./Redefine.js").redefinePlugin(on, config);
+      console.log("received spec files to run - ",  config.specPattern)
       return config;
     },
   },
