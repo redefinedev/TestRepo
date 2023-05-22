bed8dd7d4278b155f3eaab1a61e6917752057ea8
britt frenkel <britt@redefine.dev>
2023-05-22 09:35:09 +0300

    expect whole config from redefine

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,7 @@ module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       // implement node event listeners here
-      var specFiles = [];
-      specFiles = await require("./Redefine.js").redefinePlugin(on, config);
-      console.log("received spec files to run - ",  specFiles)
-      config.specPattern = specFiles;
+      config = await require("./Redefine.js").redefinePlugin(on, config);
       return config;
     },
   },
