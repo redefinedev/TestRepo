9463ab91d34c6cf26bf53d087ea42237455fb903
Ayal Regev <ayal@redefine.dev>
2023-05-23 12:49:46 +0300

    xfail and xpass

--- test_cases.py
+++ test_cases.py
@@ -1,22 +1,10 @@
 import pytest
 
 
-def test_s1():
-    assert True
+def test_xfail():
+    pytest.xfail()
 
 
-def test_f1():
-    assert False
-
-
-def test_f2():
-    assert False
-
-
-@pytest.fixture
-def fixture1():
-    raise Exception
-
-
-def test_exceptions(fixture1):
-    pass
+def test_xpass(request):
+    mark = pytest.mark.xfail(reason="xpass")
+    request.node.add_marker(mark)
