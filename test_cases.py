8e484b38fca47ce702c9746bbfde0d2e2eb04418
Ayal Regev <ayal@redefine.dev>
2023-05-23 12:39:41 +0300

    reruns test errors

--- test_cases.py
+++ test_cases.py
@@ -1,8 +1,22 @@
+import pytest
+
+
 def test_s1():
     assert True
+
 
 def test_f1():
     assert False
 
+
 def test_f2():
     assert False
+
+
+@pytest.fixture
+def fixture1():
+    raise Exception
+
+
+def test_exceptions(fixture1):
+    pass
