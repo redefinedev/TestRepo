60810bcc7bb42f0d6ba98991816529be4c771b98
britt frenkel <britt@redefine.dev>
2023-05-29 13:15:55 +0300

    added hooks to deselected file for tests

--- cypress/e2e/basic_tests/basic_test.cy.js
+++ cypress/e2e/basic_tests/basic_test.cy.js
@@ -1,4 +1,14 @@
 
+/* add hooks to check that once deselected these hooks won't run */
+
+beforeEach(() => {
+    cy.wait(500);
+});
+
+before(() => {
+    cy.wait(500);
+});
+
 describe("basic suite test", () => {
     it("first test not failing", () => {
         expect(true).to.equal(true);
