73bff8d3c31b8976c8464f3a04548e29b2148a71
nitayH <nitay@redefine.dev>
2023-05-30 12:41:26 +0300

    .

--- cypress/e2e/basic_tests/basic_test.cy.js
+++ cypress/e2e/basic_tests/basic_test.cy.js
@@ -1,19 +1,32 @@
-
-/* add hooks to check that once deselected these hooks won't run */
-
-beforeEach(() => {
-    cy.wait(500);
-});
-
-before(() => {
-    cy.wait(500);
-});
-
-describe("basic suite test", () => {
-    it("first test not failing", () => {
-        expect(true).to.equal(true);
-        cy.wait(1000);
-    });
-
-    }
-);
+describe('generated from a list', () => {
+    const operations = [
+      {
+        name:"pass0",
+        op: '2 + 2',
+        value: 4,
+      },
+      {
+        name: "pass1",
+        op: '10 - 15',
+        value: -5,
+      },
+      {
+        name: "pass2",
+        op: '3 * 17',
+        value: 51,
+      },
+      {
+          name: "fails3",
+          op: '1 + 1',
+          value: 3,
+        },
+    ]
+  
+    // dynamically create a single test for each operation in the list
+    operations.forEach((operation) => {
+      // derive test name from data
+      it(`computes ${operation.name}`, () => {
+        cy.wrap(eval(operation.op)).should('equal', operation.value)
+      })
+    })
+  })
