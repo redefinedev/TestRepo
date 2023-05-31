f8fd8cdf200a3273606a8276d8af0908ebb33157
britt frenkel <britt@redefine.dev>
2023-05-31 16:04:12 +0300

    added retry to configuration file

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.retries = 2;
       config = await require("./Redefine.js").redefinePlugin(on, config);
       console.log("cypress spec pattern: ", config.specPattern)
       return config;
