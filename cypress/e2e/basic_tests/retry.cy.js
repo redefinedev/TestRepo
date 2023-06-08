2c7f0f7b213c88942e163521bd69cd000001ea99
britt frenkel <britt@redefine.dev>
2023-06-08 13:45:43 +0300

    added success in retry

--- cypress/e2e/basic_tests/retry.cy.js
+++ cypress/e2e/basic_tests/retry.cy.js
@@ -1,8 +1,12 @@
 
 
+var index = 0;
 describe("suite that has retry test", () => {
+    beforeEach(() => {
+        index++;
+    });
     it("retry test", () => {
-        expect(true).to.equal(false);
+        expect(index).to.equal(3);
     });
 
     it("doesn't retry", () => {
