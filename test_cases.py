fd20a80a2ac549413fd03b55422f199350603eab
Ayal Regev <ayal@redefine.dev>
2023-06-26 22:42:21 +0300

    1000 tests

--- test_cases.py
+++ test_cases.py
@@ -1,6 +1,6 @@
 import pytest
 from time import sleep
 
-@pytest.mark.parametrize("index", [i for i in range(100)])
+@pytest.mark.parametrize("index", [i for i in range(1000)])
 def test_sleep(index):
     sleep(1)
