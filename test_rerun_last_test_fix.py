ed684a050110652930e350449c632c7fc112ca4a
Yuval Shimon <yuvalshi0@gmail.com>
2023-07-11 18:03:43 +0300

    rerun support tests

--- test_rerun_last_test_fix.py
+++ test_rerun_last_test_fix.py
@@ -6,11 +6,11 @@ INDEX = 0
 # should pass on the second run
 # meaning it should run twice
 def test_passed_rerun():
-    sleep(1)
     global INDEX
     INDEX += 1
     if INDEX == 1:
         assert False
+    sleep(1)
     assert True
 
 
