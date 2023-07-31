c65c640ee554210a41e709de0f1b2b7761decc45
Yuval Shimon <yuvalshi0@gmail.com>
2023-07-31 10:59:28 +0300

    adding failing and passing tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
+    async setupNodeEvents(on, config) {
+      config = await require("./Redefine.js").redefinePlugin(on, config);
       // implement node event listeners here
       return config
     },
