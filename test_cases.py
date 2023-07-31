0439dd5e07494e4ecf080aa69fdee0f8b30390fa
Yuval Shimon <yuvalshi0@gmail.com>
2023-07-31 10:25:57 +0300

    adding more failng and passing tests

--- test_cases.py
+++ test_cases.py
@@ -3,8 +3,14 @@ import pytest
 def test_passes():
   pass
 
+def test_passes2():
+  pass
+
 def test_fails():
   assert 1 == 0
+
+def test_fails2():
+  assert 1 == 2
 
 @pytest.mark.skip
 def test_skipped():
