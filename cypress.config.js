d036efcb18c43927746ad476df3d4dae6a515b40
Yuval Shimon <yuvalshi0@gmail.com>
2023-08-01 08:31:25 +0300

    removing pre installed redefine

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
-      config = await require("./Redefine.js").redefinePlugin(on, config);
       // implement node event listeners here
       return config
     },
