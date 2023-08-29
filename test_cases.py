7fd909beb6590d3c94074a7a6dceddab61ce375b
britt frenkel <britt@redefine.dev>
2023-08-29 14:51:05 +0300

    added sleep

--- test_cases.py
+++ test_cases.py
@@ -1,6 +1,8 @@
+import time
 import pytest
 
 def test_passes():
+  time.sleep(5)
   pass
 
 def test_dynamic_skip():
@@ -8,6 +10,7 @@ def test_dynamic_skip():
     pytest.skip('dynamic skip')
 
 def test_fails():
+  time.sleep(5)
   assert 1 == 0
 
 @pytest.mark.skip
