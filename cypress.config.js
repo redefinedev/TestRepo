b76489bb55f75ab257d650f1a0e91e2399dcc87e
Yuval Shimon <yuvalshi0@gmail.com>
2023-09-28 12:05:57 +0300

    lots of hooks

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,26 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      return config
+      on("after:run", (results) => {
+        console.log(results);
+      });
+      on("after:spec", (spec, results) => {
+        console.log(results);
+      });
+      on("before:browser:launch", (browser = {}, launchOptions) => {
+        console.log(browser, launchOptions);
+      });
+      on("before:run", (results) => {
+        console.log(results);
+      });
+      on("before:spec", (spec, results) => {
+        console.log(results);
+      });
+      on("task", (results) => {
+        console.log(results);
+      });
+
+      return config;
     },
   },
 });
