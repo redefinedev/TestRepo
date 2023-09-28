5cba6a9d7aa6d4800b9448b8e4846f3e05be1983
Yuval Shimon <yuvalshi0@gmail.com>
2023-09-28 10:31:00 +0300

    fix

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,8 @@ module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       // implement node event listeners here
-      replayPlugin(on, config);
       config = await require("./Redefine.js").redefinePlugin(on, config);
-
+      replayPlugin(on, config);
       console.log("cypress spec pattern: ", config.specPattern);
       return config;
     },
