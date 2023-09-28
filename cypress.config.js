8308202c11fa49fc430ad7a48c26aaadc9d0cc21
Yuval Shimon <yuvalshi0@gmail.com>
2023-09-28 12:47:24 +0300

    register the plugin in the return statment

--- cypress.config.js
+++ cypress.config.js
@@ -1,26 +1,29 @@
 const { defineConfig } = require("cypress");
 
+const registerPlugin = (on, config) => {
+  on("after:run", (results) => {
+    console.log("after run 1");
+  });
+  on("after:spec", (spec, results) => {
+    console.log("after spec 1");
+  });
+  on("before:browser:launch", (browser = {}, launchOptions) => {
+    console.log("before browser launch 1");
+  });
+  on("before:run", (results) => {
+    console.log("before run 1");
+  });
+  on("before:spec", (spec, results) => {
+    console.log("before spec 1");
+  });
+  return config;
+};
+
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      on("after:run", (results) => {
-        console.log(results);
-      });
-      on("after:spec", (spec, results) => {
-        console.log(results);
-      });
-      on("before:browser:launch", (browser = {}, launchOptions) => {
-        console.log(browser, launchOptions);
-      });
-      on("before:run", (results) => {
-        console.log(results);
-      });
-      on("before:spec", (spec, results) => {
-        console.log(results);
-      });
-
-      return config;
+      return registerPlugin(on, config);
     },
   },
 });
