0bf821e51a6ddf9fd615f23ceb032ac8858d9456
Yuval Shimon <yuvalshi0@gmail.com>
2023-09-28 09:23:03 +0300

    test

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,14 @@
 const { defineConfig } = require("cypress");
-
+const { plugin: replayPlugin } = require("@replayio/cypress");
 module.exports = defineConfig({
   e2e: {
+    
     async setupNodeEvents(on, config) {
       // implement node event listeners here
-      config = await require("./Redefine.js").redefinePlugin(on, config);
+      replayPlugin(on, config);
       console.log("cypress spec pattern: ", config.specPattern)
       return config;
     },
   },
+  chromeWebSecurity: false,
 });
