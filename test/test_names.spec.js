b6b07889d141e6d806927a0a819499e07c75cd37
britt frenkel <britt@redefine.dev>
2023-11-09 11:12:06 +0100

    removed the add function

--- test/test_names.spec.js
+++ test/test_names.spec.js
@@ -33,23 +33,23 @@ describe("Fourth Suite", () => {
     });
 });
 
-function add(...numbers) {
-    return numbers.reduce((sum, number) => {
-      return sum + number;
-    })
-}
+// function add(...numbers) {
+//     return numbers.reduce((sum, number) => {
+//       return sum + number;
+//     })
+// }
 
-describe('add()', function() {
-    var tests = [
-      {args: [1, 2], expected: 3},
-      {args: [1, 2, 3], expected: 6},
-      {args: [1, 2, 3, 4], expected: 10}
-    ];
+// describe('add()', function() {
+//     var tests = [
+//       {args: [1, 2], expected: 3},
+//       {args: [1, 2, 3], expected: 6},
+//       {args: [1, 2, 3, 4], expected: 10}
+//     ];
   
-    tests.forEach(function(test) {
-      it('correctly adds ' + test.args.length + ' args', function() {
-        var res = add.apply(null, test.args);
-        assert.equal(res, test.expected);
-      });
-    });
-   });
+//     tests.forEach(function(test) {
+//       it('correctly adds ' + test.args.length + ' args', function() {
+//         var res = add.apply(null, test.args);
+//         assert.equal(res, test.expected);
+//       });
+//     });
+//    });
