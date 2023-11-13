400a66c350818617ff53f73395da290e62c1f1d4
nitayH <nitay@redefine.dev>
2023-11-13 10:17:42 +0200

    .

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
+const { cloudPlugin } = require("cypress-cloud/plugin");
 
 module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       // implement node event listeners here
       console.log("cypress spec pattern: ", config.specPattern)
-      return config;
+      return cloudPlugin(on, config);
     },
   },
 });
