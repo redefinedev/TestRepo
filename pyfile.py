2b40dfdf22b44810e4467dbadbd4c4d942dfbb1f
Yuval Shimon <yuvalshi0@gmail.com>
2023-12-21 15:45:45 +0200

    fix

--- pyfile.py
+++ pyfile.py
@@ -1,1 +1,8 @@
-print("Hello World")
+# a script to replicate a file
+import shutil
+
+file_path = "/home/yuvals/TestRepo/cypress/e2e/basic_tests/basic_test.cy.js"
+
+for i in range(1, 1000):
+    new_file_path = file_path.replace(".cy.js","")
+    shutil.copy(file_path, new_file_path + str(i) + ".cy.js")
