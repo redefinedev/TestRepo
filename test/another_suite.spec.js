d324f8dc520fcaabbe9a7fe84b47cef54eef959f
britt frenkel <britt@redefine.dev>
2024-01-02 16:47:35 +0200

    accuracy check

--- test/another_suite.spec.js
+++ test/another_suite.spec.js
@@ -3,21 +3,7 @@
 const assert = require("assert");
 
 
-
 describe("another Suite in file", () => {
-    before(() => {
-        console.log("in before suite");
-    });
-    beforeEach(() => {
-        console.log("in before each");
-    });
-    afterEach(() => {
-        console.log("in after each");
-    });
-    after(() => {
-        console.log("in after suite");
-    });
-
     it("Test passes", () => {
         assert.equal(true, true);
     });
--- test/test_names.spec.js
+++ test/test_names.spec.js
@@ -16,15 +16,8 @@ describe("First big suite", () => {
     });
 });
 
-describe("Second big suite", () => {
-    it("Test 2", function (done)  {
-        assert.equal(9, 9);
-        done();
-        sleep(3000);
-    });
-});
 
-describe("Third big Suite contains a suite", () => {
+describe("second big Suite contains a suite", () => {
     describe("inside suite", () => {
         it("sum 7 2 ", function(done) {
             assert.equal(9, 9);
@@ -34,35 +27,3 @@ describe("Third big Suite contains a suite", () => {
 
     });
 });
-
-describe("Fourth Suite", () => {
-    it("Test fails", function (done)  {
-        assert.equal(false, true);
-        done();
-        sleep(3000);
-    });
-    it("Test passes", () => {
-        assert.equal(true, true);
-    });
-});
-
-// function add(...numbers) {
-//     return numbers.reduce((sum, number) => {
-//       return sum + number;
-//     })
-// }
-
-// describe('add()', function() {
-//     var tests = [
-//       {args: [1, 2], expected: 3},
-//       {args: [1, 2, 3], expected: 6},
-//       {args: [1, 2, 3, 4], expected: 10}
-//     ];
-  
-//     tests.forEach(function(test) {
-//       it('correctly adds ' + test.args.length + ' args', function() {
-//         var res = add.apply(null, test.args);
-//         assert.equal(res, test.expected);
-//       });
-//     });
-//    });
