047aadc331c9b6e4850c59a171f4860504341c56
nitayH <nitay@redefine.dev>
2024-01-14 13:55:45 +0200

    source branch for sorry cypress

---
+++ currents.config.js
@@ -0,0 +1,6 @@
+// currents.config.js
+module.exports = {
+    projectId: "test_proj_id", // the projectId, can be any values for sorry-cypress users
+    recordKey: "test_record_key", // the record key, can be any value for sorry-cypress users
+    cloudServiceUrl: "http://localhost:1234",   // Sorry Cypress users - set the director service URL
+  };
--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("cypress");
+const { cloudPlugin } = require("cypress-cloud/plugin");
 
 module.exports = defineConfig({
   e2e: {
@@ -6,7 +7,8 @@ module.exports = defineConfig({
       // implement node event listeners here
       config = await require("./Redefine.js").redefinePlugin(on, config);
       console.log("cypress spec pattern: ", config.specPattern)
-      return config;
+      return cloudPlugin(config);
     },
   },
+  projectId: 'test_proj_id',
 });
