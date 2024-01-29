b78633cf98e2120241ec9552919ea256d5e288b3
britt frenkel <britt@redefine.dev>
2024-01-29 16:24:03 +0200

    added more tests

---
+++ test_a.py
@@ -0,0 +1,7 @@
+import time
+
+def test_first():
+  time.sleep(1)
+
+def test_second():
+  time.sleep(1)
---
+++ test_b.py
@@ -0,0 +1,7 @@
+import time
+
+def test_third():
+  time.sleep(1)
+
+def test_fourth():
+  time.sleep(1)
