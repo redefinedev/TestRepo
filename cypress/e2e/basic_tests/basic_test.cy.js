8dc266884f14fda8b07e43ffa320dde3bd6c1a4c
nitayH <nitay@redefine.dev>
2024-03-12 14:28:51 +0200

    paramertrize

--- cypress/e2e/basic_tests/basic_test.cy.js
+++ cypress/e2e/basic_tests/basic_test.cy.js
@@ -9,11 +9,23 @@ before(() => {
     cy.wait(500);
 });
 
-describe("basic suite test", () => {
-    it("first test not failing", () => {
-        expect(true).to.equal(true);
-        cy.wait(1000);
-    });
+describe("basic parametrize suite test", () => {
+    let data = [
+        { name: "test1", age: 0 },
+        { name: "test2", age: 1 },
+        { name: "test3", age: 8 },
+        { name: "test4", age: 20 },
+        { name: "test5", age: 30 },
+        { name: "test6", age: 40 },
+    ];
+
+    data.forEach((data) => {
+        it("parametrize test", () => {
+            expect(data.age).to.be.greaterThan(10);
+            cy.wait(1000);
+        });
+    })
+    ;
 
     }
 );
