37ee7c8b3ffedf1a93fe63d954900a0afdb52e26
nitayH <nitay@redefine.dev>
2024-03-12 16:11:21 +0200

    cloud

--- some_subdir/test_project/cypress.config.js
+++ some_subdir/test_project/cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     async setupNodeEvents(on, config) {
       // implement node event listeners here
+      projectId="iqunju"
       console.log("cypress spec pattern: ", config.specPattern)
       return config;
     },
