0e3f6d5353450548550a4accc28a076c31a93e4b
nitayH <nitay@redefine.dev>
2024-03-13 17:50:24 +0200

    try

--- some_subdir/test_project/cypress.config.js
+++ some_subdir/test_project/cypress.config.js
@@ -2,11 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    projectId: "iqunju",
     async setupNodeEvents(on, config) {
       // implement node event listeners here
       console.log("cypress spec pattern: ", config.specPattern)
       return config;
     },
+    projectId: "iqunju",
   },
 });
