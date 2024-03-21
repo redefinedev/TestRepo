30d517054f34990829de6abae69cf678b469a232
Yuval Shimon <yuvalshi0@gmail.com>
2024-03-21 19:04:26 +0200

    try

--- cypress/e2e/basic_tests/skips.cy.js
+++ cypress/e2e/basic_tests/skips.cy.js
@@ -1,14 +1,6 @@
 describe("suite with failing test", () => {
-  it.skip("failing test", () => {
-    // this test should fail
-    // skip
-  });
-
   it("passing test", () => {
+    this.skip();
     expect(true).to.equal(true);
-  });
-
-  it("failing test", () => {
-    expect(true).to.equal(false);
   });
 });
